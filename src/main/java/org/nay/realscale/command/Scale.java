e8ed309c1bba78ef91308aa3ac0f098c1d5bf88b
jonakls <asmot54@hotmail.com>
2024-06-25 20:36:27 -0500

    feat(command): simplify parts of the code, add validations and add messages about player attributes

--- src/main/java/org/nay/realscale/command/Scale.java
+++ src/main/java/org/nay/realscale/command/Scale.java
@@ -1,6 +1,7 @@
 package org.nay.realscale.command;
 
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,11 +10,9 @@ import org.bukkit.entity.Player;
 import org.nay.realscale.RealScale;
 import org.nay.realscale.utils.Colorize;
 
-import java.util.Objects;
-
 public class Scale implements CommandExecutor {
 
-    private RealScale plugin;
+    private final RealScale plugin;
 
     public Scale(RealScale plugin) {
         this.plugin = plugin;
@@ -25,30 +24,47 @@ public class Scale implements CommandExecutor {
             commandSender.sendMessage("Only players can use this command");
             return true;
         }
+
         FileConfiguration config = plugin.getConfig();
         Player player = (Player) commandSender;
-        if (!player.hasPermission("scale.scale")){
+        if (!player.hasPermission("scale.scale")) {
             player.sendMessage(Colorize.Color(config.getString("Messages.no-permission")));
             return true;
         }
-        if (args.length > 0) {
-            try {
-                double maximumMeasure = config.getDouble("Config.maximum-measure"),
-                        minimumMeasure = config.getDouble("Config.minimum-measure"),
-                        measure = Double.parseDouble(args[0]),
-                        result = (measure * 0.51) / 100;
-                if (measure < maximumMeasure && measure > minimumMeasure) {
-                    Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_SCALE)).setBaseValue(result);
-                    player.sendMessage(Colorize.Color(config.getString("Messages.scale-change")).replaceAll("%scale%", String.valueOf(result)).replaceAll("%player%", player.getDisplayName()).replaceAll("%measure%", args[0]));
-                } else {
-                    player.sendMessage(Colorize.Color(config.getString("Messages.exceeded-measure")));
-                }
-            } catch (NumberFormatException e) {
-                player.sendMessage(Colorize.Color(config.getString("Messages.invalid-measure")));
-            }
-        } else {
+
+        if (args.length == 0) {
             player.sendMessage(Colorize.Color(config.getString("Messages.no-params")));
+            return false;
         }
-        return false;
+
+        try {
+            double maximumMeasure = config.getDouble("Config.maximum-measure"),
+                    minimumMeasure = config.getDouble("Config.minimum-measure"),
+                    measure = Double.parseDouble(args[0]),
+                    result = (measure * 0.51) / 100;
+            if (measure < maximumMeasure && measure > minimumMeasure) {
+
+                AttributeInstance scale = player.getAttribute(Attribute.GENERIC_SCALE);
+
+                // It may be unlikely, but it's better to be sure.
+                if (scale == null) {
+                    player.sendMessage(Colorize.Color(config.getString("Messages.no-scale-attribute")));
+                    return true;
+                }
+
+                scale.setBaseValue(result);
+                player.sendMessage(Colorize.Color(
+                        config.getString("Messages.scale-change"),
+                        "scale", String.valueOf(result),
+                        "player", player.getDisplayName(),
+                        "measure", args[0]
+                ));
+            } else {
+                player.sendMessage(Colorize.Color(config.getString("Messages.exceeded-measure")));
+            }
+        } catch (NumberFormatException e) {
+            player.sendMessage(Colorize.Color(config.getString("Messages.invalid-measure")));
+        }
+        return true;
     }
 }
