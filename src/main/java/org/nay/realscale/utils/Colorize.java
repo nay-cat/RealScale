9937d4023f24583376ec7eb0a5056407d4eaef9e
jonakls <asmot54@hotmail.com>
2024-06-25 20:32:02 -0500

    feat(utility): add utility method with replacements arguments

--- src/main/java/org/nay/realscale/utils/Colorize.java
+++ src/main/java/org/nay/realscale/utils/Colorize.java
@@ -1,10 +1,25 @@
 package org.nay.realscale.utils;
 
-
 import org.bukkit.ChatColor;
 
-public class Colorize {
-    public static String Color(String s) {
-        return ChatColor.translateAlternateColorCodes('&', s);
+public final class Colorize {
+
+    private Colorize() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String Color(String input) {
+        return ChatColor.translateAlternateColorCodes('&', input);
+    }
+
+    public static String Color(String input, String... replacements) {
+        if (replacements.length % 2 != 0) {
+            throw new IllegalArgumentException("Replacements must be even");
+        }
+        String result = input;
+        for (int i = 0; i < replacements.length; i += 2) {
+            result = result.replace(replacements[i], replacements[i + 1]);
+        }
+        return ChatColor.translateAlternateColorCodes('&', result);
     }
 }
